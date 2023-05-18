81ac2518f2842bb5d760a0fb42f2d3c74ee92249
Vitaminn171 <lyquocan171@gmail.com>
2023-05-18 21:22:48 +0700

    update

--- admin/SQL/connection.php
+++ admin/SQL/connection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost:3307","root","","phone_web_db");
+$con=mysqli_connect("localhost","root","","phone_web_db");
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
